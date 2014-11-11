01be0d0485f73b221a4af5a411faf123be3fa91f
unknown <erdmanj@JohnErdman17.MaristSchool.Local>
2014-11-11 11:26:59 -0500

    part 4

--- ErdmanToDoList/src/com/example/erdmantodolist/EditTaskActivity.java
+++ ErdmanToDoList/src/com/example/erdmantodolist/EditTaskActivity.java
@@ -6,14 +6,129 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditTaskActivity extends Activity {
-
+	private ToDoList toDoList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_task);
+		toDoList = new ToDoList(this);
+		
+		//Get Order Information from the PurchaseActivity
+		Intent intent = getIntent();
+		int taskId = intent.getIntExtra("com.example.Erdmantodolist.taskId",0);
+		if (taskId !=0) {
+			populateTaskData(taskId);
+			
+		}
 	}
+		
+	private void populateTaskData(int taskId)  {
+		//Bind to UI Fields
+		TextView textID = (TextView)findViewById(R.id.textID);
+		CheckBox checkPriority = (CheckBox)findViewById(R.id.checkBoxPriority);
+		EditText editDate = (EditText)findViewById(R.id.editDueDate);
+		EditText editTask = (EditText)findViewById(R.id.editTask);
+		CheckBox checkBoxDone = (CheckBox)findViewById(R.id.checkBoxDone);
+		
+		//Create Task
+		Task t = toDoList.getTask(taskId);
+		
+		//Display Data
+		textID.setText(String.valueOf(t.getId()));
+		checkPriority.setChecked(t.getPriority());
+		editDate.setText(t.getDate());
+		editTask.setText(t.getTaskDetails());
+		checkBoxDone.setChecked(t.getCompleted());
+		
+		
+		}
+	
+	//Handle Button Clicks
+		public void handleClick (View v)  {
+			switch (v.getId()) {
+			case R.id.buttonAddTask:
+				break;
+			case R.id.buttonEditTask:
+				break;
+			case R.id.buttonDeleteTask:
+				break;
+			case R.id.buttonMenu:
+				Intent intent = new Intent (this, MainActivity.class);
+				startActivity(intent);
+				break;
+			}
+		}
+		
+		private void editTaskData()  {
+			//Bind UI to fields
+			TextView textID = (TextView)findViewById(R.id.textID);
+			CheckBox checkPriority = (CheckBox)findViewById(R.id.checkBoxPriority);
+			EditText editDate = (EditText)findViewById(R.id.editDueDate);
+			EditText editTask = (EditText)findViewById(R.id.editTask);
+			CheckBox checkBoxDone = (CheckBox)findViewById(R.id.checkBoxDone);
+			
+			//Check if box has data:
+			if(textID.getText().toString().length()> 0) {
+				int id = Integer.valueOf(textID.getText().toString());
+				Task t = toDoList.getTask(id);
+				
+				t.setPriority(checkPriority.isChecked());
+				t.setDate(editDate.getText().toString());
+				t.setTask(editTask.getText().toString());
+				t.setCompleted(checkBoxDone.isChecked());
+				
+				toDoList.editTask(t);
+				
+				Intent intent = new Intent(this, MainActivity.class);
+				startActivity(intent);
+				
+			}
+		}
+		
+		private void addToDo()  {
+			//Bind to UI Fields
+			TextView textID = (TextView)findViewById(R.id.textID);
+			CheckBox checkPriority = (CheckBox)findViewById(R.id.checkBoxPriority);
+			EditText editDate = (EditText)findViewById(R.id.editDueDate);
+			EditText editTask = (EditText)findViewById(R.id.editTask);
+			CheckBox checkBoxDone = (CheckBox)findViewById(R.id.checkBoxDone);
+			
+			Task t = new Task();
+			
+			//Handle Date Data
+			t.setPriority(checkPriority.isChecked());
+			t.setDate(editDate.getText().toString());
+			t.setTask(editTask.getText().toString());
+			t.setCompleted(checkBoxDone.isChecked());
+			
+			Task newTask = toDoList.createTask(t);
+			
+			//Display the ID of the Task
+			textID.setText(String.valueOf(newTask.getId()));
+			//textID.setText("Test");
+			
+		}
+		
+		private void deleteTask()  {
+			//Bind UI to fields
+			TextView textID = (TextView)findViewById(R.id.textID);
+			
+			//check if box has data:
+			if (textID.getText().toString().length()> 0) {
+				int id = Integer.valueOf(textID.getText().toString());
+				Task t = toDoList.getTask(id);
+				toDoList.deleteTask(t);
+				Intent intent = new Intent(this, MainActivity.class);
+				startActivity(intent);
+			}
+		}
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,19 +149,5 @@ public class EditTaskActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	//Handle Button Clicks
-	public void handleClick (View v)  {
-		switch (v.getId()) {
-		case R.id.buttonAddTask:
-			break;
-		case R.id.buttonEditTask:
-			break;
-		case R.id.buttonDeleteTask:
-			break;
-		case R.id.buttonMenu:
-			Intent intent = new Intent (this, MainActivity.class);
-			startActivity(intent);
-			break;
-		}
-	}
+	
 }
